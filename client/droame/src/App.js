18504da7bdfffe28d8ac7d76d2d8613c835a4d4b
Gyandeep <gyandeepmehra370@gmail.com>
2023-03-29 11:49:28 +0530

    Made some changes to App.js

--- client/droame/src/App.js
+++ client/droame/src/App.js
@@ -6,6 +6,8 @@ import Register from './components/Register/Register';
 import AddCustomer from './components/addCustomer/AddCustomer';
 import UpdateCustomer from './components/updateCustomer/UpdateCustomer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AddBooking from './components/addBooking/AddBooking';
+import UpdateBooking from './components/updateBooking/UpdateBooking';
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/addcustomer" element={<AddCustomer />} />
           <Route path="/booking/updatecustomer" element={<UpdateCustomer />} />
+          <Route path="/booking/addbooking" element={<AddBooking />} />
+          <Route path="/booking/updatebooking" element={<UpdateBooking />} />
         </Routes>
       </BrowserRouter >
     </div>
